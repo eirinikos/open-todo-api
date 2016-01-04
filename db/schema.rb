8746292e8b7fbe718e58e64f2bbb26426f28d3d5
Andrea <eirinikos@gmail.com>
2016-01-03 17:22:23 -0800

    generate User model, add foreign key to List

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210165228) do
+ActiveRecord::Schema.define(version: 20160104001935) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20151210165228) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
