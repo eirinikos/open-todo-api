635d5436aac3c92aec9be588060c2a0416591ad4
Andrea <eirinikos@gmail.com>
2016-01-14 23:13:28 -0800

    define current_user

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,43 +1,8 @@
 class API::ListsController < ApiController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
   before_action :authenticated?
 
-  # GET /lists
-  # GET /lists.json
-  def index
-    @lists = List.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @lists }
-    end
-  end
-
-  # GET /lists/1
-  # GET /lists/1.json
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @list }
-    end
-  end
-
-  # GET /lists/new
-  def new
-    @list = List.new
-  end
-
-  # GET /lists/1/edit
-  def edit
-  end
-
-  # POST /lists
-  # POST /lists.json
-
   def create
-    # user = 
-    # list = user.lists.new(list_params)
-
+    list = current_user.lists.build(list_params)
     if list.save
       render json: list
     else
@@ -46,38 +11,9 @@ class API::ListsController < ApiController
     end
   end
 
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
-  def update
-    respond_to do |format|
-      if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /lists/1
-  # DELETE /lists/1.json
-  def destroy
-    @list.destroy
-    respond_to do |format|
-      format.html { redirect_to lists_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_list
-      @list = List.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def list_params
-      params.require(:list).permit(:title)
-    end
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
